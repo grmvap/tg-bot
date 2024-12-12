ef0bf5651b18ba32c0c744eef566deef0a0a3dc2
vanaandreev <temamoma@gmail.com>
2024-12-12 18:33:29 +0400

    tg bot

---
+++ src/main/java/com/example/BotApplication.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BotApplication {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Questions());
+            System.out.println("Бот запущен!");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
